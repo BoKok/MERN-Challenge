ba45fe9fc99cfa5981b6179d8e4452054bb870bd
BoKok <kokbo@yahoo.com>
2022-03-19 15:20:27 -0500

    code

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,5 +1,5 @@
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
+import React from 'react';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import {useMutation, useQuery} from '@apollo/react-hooks';
